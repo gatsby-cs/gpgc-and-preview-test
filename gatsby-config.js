090e1b831b82b358fb6fab7e5b51fe980f9b1602
Leithon English <leithonenglish@gmail.com>
2022-05-17 11:46:13 -0500

    simple updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {
+          "/*": ["Accept-Language: en-US;q=0.9,en;q=0.8"],
+        },
+      },
+    },
   ],
 }
